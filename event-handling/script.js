4b7e4418f0673241aea36f12c51888b96878cc9e
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2025-02-18 14:49:19 +0200

    event handling

--- event-handling/script.js
+++ event-handling/script.js
@@ -3,6 +3,7 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
 const clearAllTasksBtn = document.getElementById("clearAllBtn");
+const searchInput = document.getElementById("searchInput");
 
 let tasks = [];
 
@@ -35,10 +36,35 @@ function toggleTask(index) {
   displayTasks();
 }
 
+searchInput.addEventListener("input", () => {
+  const searchValue = searchInput.value.trim().toLowerCase();
+
+  const filteredTasks = tasks.filter((task) =>
+    task.text.toLowerCase().includes(searchValue)
+  );
+  if (filteredTasks.length === 0) {
+    taskList.innerHTML = "<li>No tasks found</li>";
+    return;
+  }
+  taskList.innerHTML = "";
+  filteredTasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" id="task-${index}" ${
+      task.completed ? "checked" : ""
+    }>
+    <label for="task-${index}">${task.text}</label>`;
+    li.querySelector("input").addEventListener("change", () =>
+      toggleTask(index)
+    );
+    taskList.appendChild(li);
+  });
+});
+
 function clearCompletedTasks() {
   tasks = tasks.filter((task) => !task.completed);
   displayTasks();
 }
+
 function clearAllTasks() {
   tasks = [];
   displayTasks();
